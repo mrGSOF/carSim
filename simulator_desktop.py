0a41f21af2e05e4cd1adb431251e4220b04a267a
Guy Soffer (GSOF) <gsoffer@yahoo.com>
2023-04-10 19:04:39 -0700

    Remapped the keys (q,a,z  i,o,p  space)

--- simulator_desktop.py
+++ simulator_desktop.py
@@ -8,24 +8,29 @@ class Simulator(SimClass.Simulator_base):
         """Sample the keys and update the variables of the car"""
         keys = pygame.key.get_pressed()
         if keys:
-            if keys[pygame.K_q]:
+            if keys[pygame.K_z]:
                 self.carMdl.addAcc(-self.accRate)
                 
-            elif keys[pygame.K_1]:
+            elif keys[pygame.K_a]:
+                self.carMdl.setAcc(0)
+
+            elif keys[pygame.K_q]:
                 self.carMdl.addAcc(+self.accRate)
             
+            elif keys[pygame.K_SPACE]:
+                self.carMdl.setAcc(0)
+                self.carMdl.setVel(0)
+                self.carMdl.setSteering(0)
+
             if keys[pygame.K_p]:
                 self.carMdl.addSteering(+self.steeringRate)
                 
-            elif keys[pygame.K_i]:
-                self.carMdl.addSteering(-self.steeringRate)
-
             if keys[pygame.K_o]:
                 self.carMdl.setSteering(0)
-            
-            if keys[pygame.K_z]:
-                self.carMdl.setVel(0)
 
+            elif keys[pygame.K_i]:
+                self.carMdl.addSteering(-self.steeringRate)
+            
 if __name__ == "__main__":
     sim = Simulator(fps=30, size=(600,750), imageWidth=30)
     try:
